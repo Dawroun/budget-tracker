4bf1869d79f02b8f707271e1de8cdd0941e31af0
Davron Abdukhakimov <davron2806@gmail.com>
2021-10-26 12:27:45 +0500

    added income and expense components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
+import Details from './components/Details/Details'
+
+import useStyles from './style'
 
 const App = () => {
+    const classes = useStyles();
     return (
         <div>
-            
+            <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh' }} >
+                <Grid item xs={12} sm={4}>
+                    <Details title="Income"/>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    Main
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Details title="Expense"/>
+                </Grid>
+            </Grid>
         </div>
     )
 }
